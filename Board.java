11b7de13e6189a26004691b63b54568d8d802399
kailyn0215 <brownkai24@7hills.org>
2023-09-28 12:58:33 -0400

    changed neutral board to ~ for wavy effect

--- Board.java
+++ Board.java
@@ -6,15 +6,19 @@ public class Board
     int numCols = 10;
     int[] ships = {2, 3, 3, 4, 5}; // or new int[5]
 
-    int[][] gameBoard = new int[numRows][numCols];
+    char[][] gameBoard = new char[numRows][numCols];
 
-    public void printBoard()
+    public void printBoard(boolean start)
     {
         //gameBoard[3][3] = 1;
         for(int r = 0; r < numRows; r++)
         {
             for(int c = 0; c < numCols; c++)
             {
+                if(start)
+                {
+                    gameBoard[r][c] = '~';
+                }
                 System.out.print(gameBoard[r][c] + " ");
             }
             System.out.print("\n");
@@ -27,7 +31,7 @@ public class Board
         if(start)
         {
             System.out.println("Please start placing your ships.\nDestroyer (2 pegs)\n Peg 1 Row:");
-            
+
         }
     }
 
