d989cf4379e836acaa8a53f984fbc20fb10e8f77
kailyn0215 <brownkai24@7hills.org>
2023-10-13 09:55:16 -0400

    s

--- Game.java
+++ Game.java
@@ -11,6 +11,7 @@ public class Game
         
         int mode;
         int create;
+        int x; // changes length of rows + cols
         Scanner start = new Scanner(System.in);
         do
         {
@@ -18,30 +19,22 @@ public class Game
             mode = start.nextInt();
         }
         while(mode > 3 || mode < 1);
-        if(mode == 1) // single player
+        if(mode == 3) // fast mode
         {
-            System.out.println("\n\nPlayer's Board:\n");
-            player1.printBoard(true, 10, 10);
-            System.out.println("\n\nComputer's Board:\n");
-            comp.printBoard(true, 10, 10);
+            x = 8;
         }
-        else if(mode == 2) // two player
+        else // reg game
         {
-            System.out.println("\n\nPlayer 1's Board:\n");
-            player1.printBoard(true, 10, 10);
-            System.out.println("\n\nPlayer 2's Board:\n");
-            player2.printBoard(true, 10, 10);
+            x = 10;
         }
-        else if(mode == 3) // FAST mode (single player, 8-8 grid)
-        {
-            System.out.println("\n\nPlayer's Board:\n");
-            player1.printBoard(true, 8, 8);
-            System.out.println("\n\nComputer's Board:\n");
-            player2.printBoard(true, 8, 8);
-        }
+        
     
         if(mode == 2) // if 2 player
         {
+            System.out.println("\n\nPlayer 1's Board:\n");
+            player1.printBoard(true, x, x);
+            System.out.println("\n\nPlayer 2's Board:\n");
+            comp.printBoard(true, x, x);
             do // ask p1 if they want to manually set up their board
             {
                 System.out.println("\n\nPlayer 1:\n\n1 - Manual Board Creation\n2 - Auto Board Creation");
@@ -90,6 +83,7 @@ public class Game
             }
         }
         player1.play();
+        player1.printBoard(false, 10, 10);
     }
 
     public void text() // introductory text to the game
